964397c9e8d911e8f6a1d584471d717440c6471a
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-12 13:52:32 -0600

    added table element

--- index.php
+++ index.php
@@ -6,5 +6,24 @@
 	</head>
 	<body>
 		<h1>My own webpage</h1>
+		<table>
+			<tr>
+				<th>Dogs</th>
+				<th>Cats</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Maine Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepard</td>
+			</tr>
+		</table>
+		<!-- comments here-->
 	</body>
 </html>
