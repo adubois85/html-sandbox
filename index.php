006a258afdbecfbcb2c5364d7f6d684620e9a6bd
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-12 14:31:33 -0600

    Added image

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
 				<td>German Shepard</td>
 			</tr>
 		</table>
+
 		<!-- HTML comments go here-->
 		<h2>A few of my favorite things</h2>
 		<ul>
@@ -31,11 +32,15 @@
 			<li>Video games</li>
 			<li>A special someone</li>
 		</ul>
+
 		<h2>Things to do today</h2>
 		<ol>
 			<li>Stay awake during class</li>
 			<li>Feed the dog</li>
 			<li>Make dinner</li>
 		</ol>
+
+		<h2>I <3 Stuff</h2>
+		<img src="images/saitama-ok.png" alt="ok">
 	</body>
 </html>
