78735f7a6332af070f715fea2c43323569c16d03
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-24 02:04:15 -0600

    Initial commit for weekend 2 homework

---
+++ other-projects/homework-weekend-2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Pseudo-code for Data Design</title>
+	</head>
+	<body>
+		<h1>Seller posts a new product to Etsy</h1>
+			<h2>Create class named profile</h2>
+				<p>Create local variable named profileId</p>
+				<p>Create local variable named profileAtHandle</p>
+				<p>Create local variable named profilePhoneNumber</p>
+				<p>Create local variable named profileEmail</p>
+				<p>Create local variable named profileHash</p>
+				<p>Create local variable named profileSalt</p>
+			<h2>Create class named listing</h2>
+				<p>Create local variable named listingId</p>
+				<p>Create local variable named listingProfileId</p>
+				<p>Create local variable named listingContent</p>
+				<p>Create local variable named listingDate</p>
+				<p>Create local variable named listingNumberOfItems</p>
+	</body>
+</html>
