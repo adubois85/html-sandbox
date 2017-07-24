adf2e6e2bf5daad091489ffd31c7d553938c12d5
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-24 02:27:02 -0600

    Work on fulfilling the assignment parameters

--- other-projects/homework-weekend-2.php
+++ other-projects/homework-weekend-2.php
@@ -13,11 +13,21 @@
 				<p>Create local variable named profileEmail</p>
 				<p>Create local variable named profileHash</p>
 				<p>Create local variable named profileSalt</p>
+				<p>Create method named getProfileId()</p>
+				<ul>
+					<li>This method queries the database for the profileId of the profile currently being re
+					<li>Then it returns the profileId of the profile currently being called</li>
+				</ul>
 			<h2>Create class named listing</h2>
 				<p>Create local variable named listingId</p>
 				<p>Create local variable named listingProfileId</p>
 				<p>Create local variable named listingContent</p>
 				<p>Create local variable named listingDate</p>
 				<p>Create local variable named listingNumberOfItems</p>
+				<p>Create method named postListing</p>
+				<ul>
+					<li>This method first calls the getProfileId method from the profile class</li>
+					<li>Then it </li>
+				</ul>
 	</body>
 </html>
