e56522b0fe2c862fac7c92b43322d0879715111b
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-31 11:29:05 -0600

    Attempt to correct the footer

--- other-projects/bootstrap.php
+++ other-projects/bootstrap.php
@@ -96,7 +96,13 @@
 			</div>
 		</div>
 		<footer>
-			<p>Footer Here</p>
+			<div	class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<p>Footer Here</p>
+					</div>
+				</div>
+			</div>
 		</footer>
 	</body>
 </html>
