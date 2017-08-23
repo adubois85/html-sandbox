f31fa46469bb3f0eebeb8274711da34184d101dc
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-23 17:01:12 -0600

    Further jquery testing; why does it work in code pen but not here? x2

--- other-projects/pwp-test.php
+++ other-projects/pwp-test.php
@@ -24,12 +24,8 @@
 				alert("hello");
 			}
 
-			$(".fa-caret-up").onclick(function() {
-				alert("hello");
-			})
-
 			$(".fa-caret-down").on("click", function() {
-				$(".section-test").toggle(1000);
+				$("section-").toggle(2000);
 			})
 		</script>
 
@@ -41,7 +37,7 @@
 			<div class="container">
 				<div class="row justify-content-center p-0">
 					<button type="button" class="btn btn-link" id="up-button">
-						<i class="fa fa-caret-left align-self-start border pt-0" onclick="toggleTop()"
+						<i class="fa fa-caret-up align-self-start border pt-0" onclick="toggleTop()"
 							aria-hidden="true"></i>
 					</button>
 					<a class="btn btn-default border fa fa-caret-up d-inline" href="#"></a>
@@ -121,7 +117,7 @@
 		<!-- <script src="scripts/script.js"></script> -->
 
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	</body>
--- other-projects/pwp-test2.php
+++ other-projects/pwp-test2.php
@@ -5,6 +5,12 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!--		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>-->
+
 		<!-- Font Awesome -->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 		<!-- Custom CSS -->
@@ -13,17 +19,23 @@
 		<title>PWP Test 2</title>
 	</head>
 	<body>
+
 		<script>
 		$(".fa-caret-down").on("click", function() {
 		$(".section-test").toggle(1000);
 		})
 		</script>
 
-		<a class="btn btn-default border fa fa-caret-down d-inline" href="#"></a>
-		<button class="fa fa-caret-up"></button>
 
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<div class="container">
+			<div class="row">
+				<a class="btn btn-default border fa fa-caret-down" href="#"></a>
+				<button class="fa fa-caret-up section-test"></button>
+			</div>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!--		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>-->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	</body>
 </html>
