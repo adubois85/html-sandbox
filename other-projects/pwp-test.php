3476265be75938ddad5fe609de19d3258a8fb22f
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-23 11:03:40 -0600

    copied over code / styling from main PWP for testing

--- other-projects/pwp-test.php
+++ other-projects/pwp-test.php
@@ -18,3 +18,35 @@
 		<title>Alexander DuBois</title>
 	</head>
 	<body>
+		<section class="section-main">
+			<div class="container">
+				<div class="row justify-content-center p-0">
+					<button type="button" class="btn btn-link" id="up-button">
+						<i class="fa fa-caret-up align-self-start border pt-0" onclick="toggleTop()"
+							aria-hidden="true"></i>
+					</button>
+					<a class="btn btn-default border fa fa-caret-up d-inline" href="#"></a>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row justify-content-center">
+					<i class="fa fa-caret-left col-xs-4 col-md-1 border p-0" aria-hidden="true"></i>
+					<p class="col-xs-4 col-md-2 align-self-center border" >This is my webpage.  Click on the arrows to
+						navigate.</p>
+					<i class="fa fa-caret-right col-xs-4 col-md-2 border" aria-hidden="true"></i>
+				</div>
+				<div class="row justify-content-center">
+					<i class="fa fa-caret-down border" aria-hidden="true"></i>
+				</div>
+			</div>
+		</section>
+
+		<!-- Optional JavaScript -->
+		<!-- <script src="scripts/script.js"></script> -->
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	</body>
+</html>
