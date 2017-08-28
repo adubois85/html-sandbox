ead9ca5be7cf96caa938e5e2b078f55e91d85861
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-28 15:11:59 -0600

    removed link to Animate.css as I am no longer using it

--- other-projects/pwp-test2.php
+++ other-projects/pwp-test2.php
@@ -12,10 +12,6 @@
 		<link	type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
 				  rel="stylesheet" />
 
-		<!-- Animate.CSS -->
-		<link rel="stylesheet"
-				href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-
 		<!-- Custom CSS -->
 		<link type="text/css" href="../stylesheets/pwp-style.css" rel="stylesheet" />
 	</head>
@@ -26,52 +22,75 @@
 				<a class="btn btn-default border fa fa-caret-down" href="#"></a>
 				<i class="fa fa-caret-up section-test"></i>
 				<button type="button" class="btn btn-primary" onclick="testAlert()">Click Me!</button>
+			</div> -->
+
+			<!--		 This is the main section; it acts as a navigation hub for the other sections -->
+			<section class="section-main">
+				<div class="container">
+					<div class="row justify-content-center">
+						<i class="fa fa-caret-up border" id="test-slideout"></i>
+					</div>
+					<div class="row justify-content-center">
+						<a class="btn btn-default border fa fa-caret-left col-xs-3" href="#"></a>
+						<p class="border col-xs-3">This is my webpage.  Click on the arrows to navigate.</p>
+						<a class="btn btn-default border fa fa-caret-right col-xs-3" href="#"></a>
+					</div>
+					<div class="row justify-content-center">
+						<a class="btn btn-default border fa fa-caret-down d-inline" href="#"></a>
+					</div>
+				</div>
+			</section>
+
+				<!-- pulled from the main PWP file -->
+	<!--			<i class="fa fa-caret-left" aria-hidden="true"></i>-->
+
+
+			<section>
+
+				<!-- Adapted from the demo here, building a modal box with straight HTML and CSS
+				https://www.webdesignerdepot.com/2012/10/creating-a-modal-window-with-html5-and-css3/ -->
+
+			<!--	<i class="fa fa-caret-down btn btn-primary"></i>
+
+				<div id="openModal" class="modalDialog hidden modal-test">
+					<div>
+						<a href="#close" title="Close" class="close">X</a>
+						<h2>Modal Box</h2>
+						<p>This is a sample modal box that can be created using the powers of CSS3.</p>
+						<p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<span id='clickableAwesomeFont' class="fa fa-caret-up border"></span>
+			</section>
+		</div>
+
+
+		<div class="container">
+			<div class="row">
+				<button class="btn btn-lg">Button</button>
+				<p class="inactive" id="#job-details-flyout-overlay">
+					Bacon ipsum dolor amet burgdoggen ball tip corned beef hamburger jowl, frankfurter chuck cupim strip steak. Beef swine venison biltong capicola, shank shankle short loin picanha pancetta pork chop pork belly jowl jerky frankfurter. Bacon spare ribs swine, salami kielbasa ball tip alcatra hamburger tri-tip boudin venison pork turkey. Boudin rump ball tip beef fatback leberkas shoulder turducken hamburger ground round sausage venison alcatra. Meatloaf prosciutto meatball tail. Leberkas burgdoggen tri-tip tail jowl.
+				</p>
+
+"
+			</div>
+		</div> -->
+	<section>
+		<div class="container">
+			<div class="row justify-content-center">
+				<a href="#" class="fa fa-caret-down btn btn-default"></a>
+			</div>
+			<div id="slideout">
+				<form>
+					<textarea class="form-control"></textarea>
+					<input class="btn btn-primary" type="submit" value="Post feedback"></input>
+				</form>
 			</div>
 		</div>
-		-->
-
-
-		<section class="section-main">
-			<div class="container">
-				<div class="row justify-content-center p-0">
-					<button type="button" class="btn btn-link" id="up-button">
-						<i class="fa fa-caret-up align-self-start border pt-0" onclick="alertTest()" aria-hidden="true"></i>
-					</button>
-					<a class="btn btn-default border fa fa-caret-up d-inline" href="#"></a>
-				</div>
-			</div>
-			<div class="container">
-				<div class="row justify-content-center">
-					<i class="fa fa-caret-left col-xs-4 col-md-1 border p-0" aria-hidden="true"></i>
-					<p class="col-xs-4 col-md-2 align-self-center border" >This is my webpage.  Click on the arrows to
-						navigate.</p>
-					<i class="fa fa-caret-right col-xs-4 col-md-2 border" aria-hidden="true"></i>
-				</div>
-				<div class="row justify-content-center">
-					<i class="fa fa-caret-down border" aria-hidden="true"></i>
-				</div>
-			</div>
-
-			<!-- pulled from the main PWP file -->
-<!--			<i class="fa fa-caret-left" aria-hidden="true"></i>-->
-
-		</section>
-
-		<section>
-
-			<a href="#openModal">Open Modal</a>
-
-			<div id="openModal" class="modalDialog animated fadeInDownBig">
-				<div>
-					<a href="#close" title="Close" class="close">X</a>
-					<h2>Modal Box</h2>
-					<p>This is a sample modal box that can be created using the powers of CSS3.</p>
-					<p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
-				</div>
-			</div>
-
-		</section>
-
+	</section>
 
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,16 @@ $(".fa-caret-down").on("click", function() {
 function testAlert() {
 	alert("Hello");
 }
+
+// $(".fa-caret-up").mouseover(function() {
+// 	$(".fa-caret-up").css("");
+// })
+//
+
+$('#test-slideout').click(function(){
+	$('#slideout').toggleClass('on');
+});
+
+$('.fa-caret-down').click(function(){
+	$('#slideout').toggleClass('on');
+});
