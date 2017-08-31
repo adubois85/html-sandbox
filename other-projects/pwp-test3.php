37683eee29392b492aba6dab70d2cc89cedebe8d
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-31 08:34:20 -0600

    created basic html doc for testing next iteration of the PWP

---
+++ other-projects/pwp-test3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Latest Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+		<!--Font Awesome -->
+		<link	type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+				  rel="stylesheet" />
+
+		<!-- Custom CSS
+		<link type="text/css" href="../stylesheets/pwp-style.css" rel="stylesheet" /> -->
+
+	</head>
+	<body>
+
+	</body>
+</html>
